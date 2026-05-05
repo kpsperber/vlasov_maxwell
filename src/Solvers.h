bd88e7ca49aac92b72152c4ffe8e696507b99e26
Byron-Selvage <bselvage@mines.edu>
2026-05-04 22:44:40 -0600

    Add Backward Euler Test (E=0)

--- src/Solvers.h
+++ src/Solvers.h
@@ -4,6 +4,8 @@
 #include "Mesh2D.h"
 #include "ScalarField.h"
 
+class DistributionFunction;
+class VectorField;
 
 /**
  * Solve the Poisson equation for a given charge density field and voltage boundary conditions.
@@ -15,5 +17,17 @@
  * @param voltage Scalar field representing voltage to be solved for. This is modified in-place.
  */
 void poisson(const Mesh2D& mesh, const ScalarField& density, ScalarField& voltage);
+
+/**
+ * @brief Backward Euler step for Vlasov equation.
+ *
+ * @param mesh Mesh2D defining the spatial and velocity mesh
+ * @param f_old Distribution at the beginning of the step
+ * @param f_new Distribution after the step (buffer to write into)
+ * @param E Electric field
+ * @param dt Time step
+ * @param qm Charge-to-mass ratio
+ */
+void backward_euler(const Mesh2D& mesh, const DistributionFunction& f_old, DistributionFunction& f_new, const VectorField& E, double dt, double qm);
 
 #endif // SOLVERS_H
